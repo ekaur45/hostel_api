8b4160b625cf182a057266b9659bc7ba36f49f7f
= <=>
2024-05-31 22:08:53 +0500

    .

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+import http from 'http';
 var express = require('express');
 var jsonServer = require('json-server');
 
 var server = express();
+server.use('/api', jsonServer.router('db.json'));
+http.createServer(server).listen(300);
 // ...
 
 // You may want to mount JSON Server on a specific end-point, for example /api
 // Optiona,l except if you want to have JSON Server defaults
 // server.use('/api', jsonServer.defaults()); 
-server.use('/api', jsonServer.router('db.json'));
 
-server.listen(3000);
+//server.listen(3000);
 
 
 // import http from 'http';
